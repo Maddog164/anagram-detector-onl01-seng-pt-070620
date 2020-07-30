ade14de03db7d53aa4f3e7511b7aabd206a1c624
Denise Henderson <denise@henderson3.us>
2020-07-30 02:14:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,23 +9,14 @@ class Anagram
   end
 
   def match(array)
-    #turn @word into a sorted array of letters
-    #turn each word in array into a sorted array of letters
-    #compare sorted array of @word to each sorted element of array
-    #return the original values of the matches
-    #word = @word.split("")
-    #ret_word = ""
     hold_result = []
     array.each do |array_word|
-      #binding.pry
       if array_word.split("").sort == @word.split("").sort
         hold_result << array_word
-        #binding.pry
-      end
+      end #if
     end #do
     return hold_result
   end
-
 
 end
 
