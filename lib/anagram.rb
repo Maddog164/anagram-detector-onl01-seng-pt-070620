1ffcc0a7fea618e91d3d30758c63c5a50b16b3b8
Denise Henderson <denise@henderson3.us>
2020-07-30 02:13:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,10 +17,10 @@ class Anagram
     #ret_word = ""
     hold_result = []
     array.each do |array_word|
-      binding.pry
+      #binding.pry
       if array_word.split("").sort == @word.split("").sort
         hold_result << array_word
-        binding.pry
+        #binding.pry
       end
     end #do
     return hold_result
