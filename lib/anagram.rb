fe2a0f435c01b375a6d3e5bc1e55eeeb07af5a61
Denise Henderson <denise@henderson3.us>
2020-07-30 01:55:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,6 +4,16 @@ class Anagram
   attr_accessor :word
 
   def match(array)
+    #turn @word into a sorted array of letters
+    #turn each word in array into a sorted array of letters
+    #compare sorted array of @word to each sorted element of array
+    #return the original values of the matches
+    #word = @word.split("")
+    #ret_word = ""
+    array.each.detect do |array_word|
+      array_word.split("").sort == @word.split("").sort
+      
+      end #do
 
 
 
